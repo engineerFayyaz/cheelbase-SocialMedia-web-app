ffa129865f42c27428e0377989d4d3b4c003123f
Bilal484 <abilalbro007@gmail.com>
2024-01-23 23:54:57 -0800

    Bilal-Gh/chattingpages

---
+++ src/Pages/Verificationbusiness/index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Header from "../../Componenets/Header";
+import '../Verificationbusiness/Verificationbusiness.css';
+
+const Verificationbusiness = () => {
+
+    return (
+        <>
+        <Header/>
+
+        <div className="container-fluid verification-identity-main">
+  <div className="row pt-5  verification-identity ">
+    <div className="col-md verification-content ">
+      <a href="javascript:void(0);" className="back-arrow" onclick="goBack()">
+        <i className="fa-solid fa-arrow-left back-arrow" />
+      </a>
+      <div className="profile-title-box ">
+        <div className=" content">
+          <h2 className=" selfie">Business manager</h2>
+          <div className="manager-name d-flex ">
+            <div className="username ">
+              Hi{" "}
+              <span className="user">
+                {"{"}username{"}"}
+              </span>
+            </div>
+            <div className="left-border"></div>
+            <div className="col-lg-9 ads">
+              Runing ads on cheelbase is much important as fueling your car for
+              a successful race. With this we would prefer dealing with Lega and
+              verified business/company before getting started with ads
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="id-information">
+        <div className="next">
+          <a href="../business-verification/business-verification.html">
+            <button className="btn next-button rounded-pill ">
+              <p>manage</p>
+            </button>
+          </a>
+        </div>
+      </div>
+      <div className="universe">
+        <h6 className="from ">From</h6>
+        <h6 className="annex-universe">Annex universe Itd</h6>
+      </div>
+    </div>
+  </div>
+</div>
+
+        </>
+    );
+}
+
+export default Verificationbusiness;
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BusinessVerification from "./Pages/Verificationsixth";
 // import HomePage from "./Pages/Home";
 // import Home from "./Pages/Home";
 // import NotFound from "./Pages/NotFound";
@@ -11,6 +12,30 @@ const Welcome = React.lazy(()=>import('./Pages/Welcome'));
 const QrCode = React.lazy(()=>import('./Pages/QrCode'));
 const LinkingDevice = React.lazy(()=>import('./Pages/LinkingDevice'));
 const Profile= React.lazy(()=>import ('./Pages/Profile'));
+const Verification1 = React.lazy(()=>import('./Pages/Verification1'));
+const Verificationsecond = React.lazy(()=>import('./Pages/Verificationsecond'));
+const Verificationthird = React.lazy(()=>import('./Pages/Verificationthird'));
+const Verificationfourth = React.lazy(()=>import('./Pages/Verificationfourth'));
+const Verificationfifth = React.lazy(()=>import('./Pages/Verificationfifth'));
+const Verificationbusiness = React.lazy(()=>import('./Pages/Verificationbusiness'));
+const Verificationsixth = React.lazy(()=>import('./Pages/Verificationsixth'));
+const Verificationid = React.lazy(()=>import('./Pages/Verificationid'));
+const Verificationconfirmation = React.lazy(()=>import('./Pages/Verificationconfirmation'));
+const Chattingpage1 = React.lazy(()=>import('./Pages/Chattingpages/Chattingpage1'));
+const Searchcontactpage = React.lazy(()=>import('./Pages/Chattingpages/Searchcontactpage'));
+const Chattingmessenger = React.lazy(()=>import('./Pages/Chattingpages/Chattingmessenger'));
+const Groupcall = React.lazy(()=>import('./Pages/Chattingpages/Groupcall'));
+const Groupcall2 = React.lazy(()=>import('./Pages/Chattingpages/Groupcall2'));
+
+const Callingpage = React.lazy(()=>import('./Pages/Chattingpages/Callingpage'));
+const Callingmarina = React.lazy(()=>import('./Pages/Chattingpages/Callingmarina'));
+const Callingvideo = React.lazy(()=>import('./Pages/Chattingpages/Callingvideo'));
+const Callingvideo2 = React.lazy(()=>import('./Pages/Chattingpages/Callingvideo2'));
+
+const Sharedscreen = React.lazy(()=>import('./Pages/Chattingpages/Sharedscreen'));
+const Groupchat = React.lazy(()=>import('./Pages/Chattingpages/Groupchat'));
+const Groupchatsecond = React.lazy(()=>import('./Pages/Chattingpages/Groupchatsecond'));
+const Groupvideosecond = React.lazy(()=>import('./Pages/Chattingpages/Groupvideosecond'));
 
 
 const ProjectRoutes =()=>{
@@ -25,6 +50,32 @@ const ProjectRoutes =()=>{
         <Route path="/QrCode" element={<QrCode/>}/>
         <Route path="/LinkingDevice" element={<LinkingDevice/>}/>
         <Route path="/Profile" element={<Profile/>}/>
+        <Route path="/Verification1" element={<Verification1/>}/>
+        <Route path="/Verificationsecond" element={<Verificationsecond/>}/>
+        <Route path="/Verificationthird" element={<Verificationthird/>}/>
+        <Route path="/Verificationfourth" element={<Verificationfourth/>}/>
+        <Route path="/Verificationfifth" element={<Verificationfifth/>}/>
+        <Route path="/Verificationbusiness" element={<Verificationbusiness/>}/>
+        <Route path="/Verificationsixth" element={<Verificationsixth/>}/>
+        <Route path="/Verificationid" element={<Verificationid/>}/>
+        <Route path="/Verificationconfirmation" element={<Verificationconfirmation/>}/>
+        <Route path="/Chattingpage1" element={<Chattingpage1/>}/>
+        <Route path="/Searchcontactpage" element={<Searchcontactpage/>}/>
+        <Route path="/Chattingmessenger" element={<Chattingmessenger/>}/>
+        <Route path="/Groupcall" element={<Groupcall/>}/>
+        <Route path="/Groupcall2" element={<Groupcall2/>}/>
+        
+        <Route path="/Callingpage" element={<Callingpage/>}/>
+        <Route path="/Callingmarina" element={<Callingmarina/>}/>
+        <Route path="/Callingvideo" element={<Callingvideo/>}/>
+        <Route path="/Callingvideo2" element={<Callingvideo2/>}/>
+        
+        <Route path="/Sharedscreen" element={<Sharedscreen/>}/>
+        <Route path="/Groupchat" element={<Groupchat/>}/>
+        <Route path="/Groupchatsecond" element={<Groupchatsecond/>}/>
+        <Route path="/Groupvideosecond" element={<Groupvideosecond/>}/>
+                    
+
         </Routes>
         </Router>
     </React.Suspense>
